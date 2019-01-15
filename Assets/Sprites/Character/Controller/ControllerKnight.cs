163964261c3dfc750c11b9a338be8154ea2bc1ab
Gustavo Gabriel Marinsalda <gabriel.marinsalda@juantoselli.com>
2019-01-15 18:41:45 -0300

    Attack

--- Assets/Sprites/Character/Controller/ControllerKnight.cs
+++ Assets/Sprites/Character/Controller/ControllerKnight.cs
@@ -2,31 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControllerKnight : MonoBehaviour {
-    private Rigidbody2D rb;
-    private Animator anim;
-    private float move;
-    private bool facingRight = true;
-    
-	void Start () {
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-	}
-	void Update ()
-    {
+public class ControllerKnight : MonoBehaviour
+{
+	private Rigidbody2D rb;
+	private Animator anim;
+	private SpriteRenderer rend;
+	private float move;
+	private bool facingRight = true;
+	bool flip = false;
 
-        if (move > 0 && !facingRight)
-            Flip();
-        else if (move < 0 && facingRight)
-        {
-            Flip();
-        }
-	 }
-    void Flip()
-    {
-        facingRight = !facingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		rend = GetComponent<SpriteRenderer>();
+	}
+	void Update()
+	{
+		if (Input.GetKey("right"))
+		{
+			flip = false;
+			rend.flipX = flip;
+			anim.SetBool("hitting", true);
+		}
+		if (Input.GetKey("left"))
+		{
+			flip = true;
+			rend.flipX = flip;
+			anim.SetBool("hitting", true);
+		}
+
+		if (!Input.GetKey("left") && !Input.GetKey("right"))
+		{
+			anim.SetBool("hitting", false);
+		}
+
+		if (move > 0 && !facingRight)
+			Flip();
+		else if (move < 0 && facingRight)
+		{
+			Flip();
+		}
+	}
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
 }
