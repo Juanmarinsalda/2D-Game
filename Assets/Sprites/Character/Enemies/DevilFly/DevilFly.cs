f2b0cd89fb8fd4a6cd0d3b487ae3ace5740d1bff
Juanmarinsalda <jmarinsalda@gmail.com>
2019-01-17 06:36:02 -0300

    DevilFly Script

---
+++ Assets/Sprites/Character/Enemies/DevilFly/DevilFly.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevilFly : MonoBehaviour
+{
+    bool stop = false;
+    bool trigger = false;
+
+    void Update()
+    {
+        if(stop == false)
+        {
+            gameObject.GetComponent<Animator>().SetBool("Start", true);
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1f *Time.deltaTime, gameObject.transform.position.y - 0.5f * Time.deltaTime, gameObject.transform.position.z);
+        }
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            stop = true;
+            gameObject.GetComponent<Animator>().SetBool("Attack", true);
+        }
+        if (collision.transform.tag == "Enemy")
+        {
+
+        }
+    }
+}
---
+++ Assets/Sprites/Character/Enemies/DevilFly/DevilFlySpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevilFlySpawner : MonoBehaviour
+{
+    float timer;
+    int EnemyCant = 0;
+    public GameObject Enemy;
+    public int MaxEnemiesPerLvl;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= 5f && EnemyCant <= MaxEnemiesPerLvl)
+        {
+            timer = 0;
+            Instantiate(Enemy);
+            EnemyCant++;
+        }
+    }
+}
